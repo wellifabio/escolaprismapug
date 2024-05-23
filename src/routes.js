8f192bee10d8ae26a7d26b02767d75d2cdbe202f
wellifabio <robsonbacchindesouza@gmail.com>
2024-05-23 11:20:40 -0300

    aluno

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const turma = require('./controllers/turma')
+const aluno = require('./controllers/aluno')
 
 router.get('/', turma.iniciar)
+router.get('/aluno', aluno.iniciar)
 
 module.exports = router
