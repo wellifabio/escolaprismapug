98c610f3b8c8e69d349f33ac28889ed5cc7f4438
wellifabio <wellifabio@hotmail.com>
2024-05-28 17:03:16 -0300

    turmas

--- src/controllers/professor.js
+++ src/controllers/professor.js
@@ -2,7 +2,18 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 const iniciar = async (req, res) => {
-    const professores = await prisma.professor.findMany({});
+    const professores = await prisma.professor.findMany({
+        select: {
+            id: true,
+            nome: true,
+            especialidade: true,
+            turmas: {
+                select: {
+                    turma: true,
+                }
+            }
+        }
+    });
     res.render('professor', { professores: professores });
 }
 
--- src/controllers/turma.js
+++ src/controllers/turma.js
@@ -2,11 +2,29 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 const iniciar = async (req, res) => {
-    const turmas = await prisma.turma.findMany({});
+    const turmas = await prisma.turma.findMany({
+        select: {
+            id: true,
+            nome: true,
+            abreviacao: true,
+            alunos: {
+                select: {
+                    ra: true,
+                    nome: true,
+                    nascimento: true
+                }
+            },
+            professores: {
+                select: {
+                    professor: true,
+                }
+            },
+        }
+    });
     res.render('index', { turmas: turmas });
 }
 
-const create = async(req, res)=>{
+const create = async (req, res) => {
     const turma = await prisma.turma.create({
         data: req.body
     });
